593938499bd4bd1f73433f518aab01cb4686d533
rhysrevans3 <34507919+rhysrevans3@users.noreply.github.com>
2022-08-16 11:00:23 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,24 +1,21 @@
 {
+  // This sets jupyter-notebook as the workbench for ipynb files.
   "workbench.editorAssociations": {
     "*.ipynb": "jupyter-notebook"
   },
-  "notebook.cellToolbarLocation": {
-    "default": "right",
-    "jupyter-notebook": "left"
-  },
-  // This sets my default tab size to 4
+  // This sets the default tab size to 4
   "editor.tabSize": 4,
-  // This sets my tab size to 2 for Javascript
+  // This sets the tab size to 2 for Javascript
   "[javascript]": {
     "editor.tabSize": 2
   },
-  // This sets my tab size to 2 for Javascript
+  // This sets the tab size to 2 for Javascript
   "[json]": {
     "editor.tabSize": 2
   },
   // This forces the tab to be **space**
   "editor.insertSpaces": true,
-  // This set black as my code formatter
+  // This set black as the code formatter
   "python.formatting.provider": "black",
   // This means my code will be formatted on save
   "editor.formatOnSave": true,
